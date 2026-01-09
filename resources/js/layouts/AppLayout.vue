2fd375aeec2a203942a835bf5f1931bc1fc5ce86
Ufooo <ufooo@netipar.hu>
2026-01-09 21:28:30 +0100

    Simplify frontend app layout structure

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue';
+import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
 import FlashMessages from '@/components/FlashMessages.vue';
 import ConfirmationDialog from '@/components/shared/ConfirmationDialog.vue';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { Sonner } from '@/components/ui/sonner';
-import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import type { BreadcrumbItemType } from '@/types';
+import { usePage } from '@inertiajs/vue3';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -12,12 +15,22 @@ interface Props {
 withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
 });
+
+const isOpen = usePage().props.sidebarOpen as boolean;
 </script>
 
 <template>
-    <AppLayout :breadcrumbs="breadcrumbs">
-        <slot />
-    </AppLayout>
+    <SidebarProvider :default-open="isOpen" class="min-h-screen bg-background">
+        <AppSidebar />
+        <SidebarInset
+            class="flex-1 overflow-x-hidden rounded-2xl border border-border bg-card p-6"
+        >
+            <AppSidebarHeader :breadcrumbs="breadcrumbs" />
+            <div class="flex-1">
+                <slot />
+            </div>
+        </SidebarInset>
+    </SidebarProvider>
     <ConfirmationDialog />
     <FlashMessages />
     <Sonner position="top-right" :duration="5000" />
