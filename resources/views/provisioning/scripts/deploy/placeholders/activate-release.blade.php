bb46e1ffda0b8c300a9147d0053d354248ff7a81
Ufooo <ufooo@netipar.hu>
2026-01-13 09:32:31 +0100

    Fix working directory after purging old releases

--- resources/views/provisioning/scripts/deploy/placeholders/activate-release.blade.php
+++ resources/views/provisioning/scripts/deploy/placeholders/activate-release.blade.php
@@ -5,3 +5,5 @@ echo -e '\e[32m=> Purging old releases\e[0m'
 cd "$NIP_RELEASES_PATH"
 CURRENT_RELEASE=$(readlink -f "$NIP_SITE_ROOT/current" | xargs basename)
 ls -t | tail -n +6 | grep -v "^${CURRENT_RELEASE}$" | xargs -r rm -rf
+
+cd "$NIP_SITE_PATH"
